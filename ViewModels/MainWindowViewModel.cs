178cd37e021bd8cb8fac533983988e868a883462
Александр Каменских <aldricBT@yandex.ru>
2023-08-28 09:48:54 +0500

    Создание инфраструктуры / тесты для проверки

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using DataGrid_test.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid_test.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Title
+        private string _title = "flflfl";
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
+
+        //#region TitleBox
+        //private string _title_text;
+        //public string Title_text
+        //{
+        //    get => _title_text;
+        //    set => Set(ref _title_text, value);
+        //}
+        //#endregion
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+    }
+}
